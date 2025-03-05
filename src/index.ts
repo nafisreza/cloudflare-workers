d366487ff23b2444dfdfd4fcd541b733e9d5e91a
nafisreza <bigganinafis@gmail.com>
2025-03-06 00:43:17 +0600

    post req with body, header and query params

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,13 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello from cloudflare workers!')
+app.post('/', async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log('req header: ',c.req.header("Authorization"));
+  console.log('req param: ',c.req.query("param"));
+
+  return c.text('Hello Hono!')
 })
 
 export default app
